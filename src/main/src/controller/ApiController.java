cb50ce75801e3b24f0753ac4e27bdb9e59af5cd5
张三 <zhangsan@git.com>
2020-07-12 18:05:47 +0800

    提交全部

---
+++ src/main/src/controller/ApiController.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class ApiController {
+
+    public  static void main(String[] args){
+        System.out.print(123);
+    }
+
+    public void fun1(){
+        System.out.print(123123123);
+    }
+
+}
+
+
+
+
